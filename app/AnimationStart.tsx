7afe08bb86f14cc245e46c0a1a9973516cd12627
Theo-Coquio <coquiotheo@gmail.com>
2025-03-08 00:44:22 +0100

    added gradient background moving with mouse

--- app/AnimationStart.tsx
+++ app/AnimationStart.tsx
@@ -5,54 +5,34 @@ import gsap from "gsap";
 
 const AnimationStart = () => {
   useEffect(() => {
-    const chars = document.querySelectorAll(".portfolio-char");
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const xPercent = (clientX / width) * 100;
+      const yPercent = (clientY / height) * 100;
 
-    gsap.set(chars, { autoAlpha: 1 });
+      const gradient = `radial-gradient(circle at ${xPercent}% ${yPercent}%, rgba(255,255,255,0.8), rgba(0,123,255,0.2))`;
 
-    gsap.from(chars, {
-      duration: 1,
-      y: 100,
-      autoAlpha: 0,
-      stagger: 0.1,
-    });
-
-    chars.forEach((char) => {
-      gsap.to(char, {
-        duration: 2 + Math.random(), // Random duration between 2 and 3 seconds
-        y: -20 + Math.random() * 40, // Random y position between -20 and 20
-        x: -10 + Math.random() * 20, // Random x position between -10 and 10
-        repeat: -1,
-        yoyo: true,
-        ease: "power1.inOut",
+      gsap.to(".background", {
+        background: gradient,
+        duration: 0.5,
+        ease: "power1.out",
       });
-    });
+    };
 
-    // Floating effect
-    chars.forEach((char) => {
-      gsap.to(char, {
-        duration: 3 + Math.random() * 2, // Random duration between 3 and 5 seconds
-        y: "+=10", // Move down by 10px
-        x: "+=5", // Move right by 5px
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
-      });
-    });
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
-
-  const portfolio = "PORTFOLIO";
 
   return (
     <div
-      className={`w-full h-full bg-black flex flex-col items-center justify-center text-[200px] ${aboreto.className}`}
+      className={`w-full h-full background flex flex-col items-center justify-center text-[200px] ${aboreto.className}`}
     >
-      <h1 className="">
-        {portfolio.split("").map((letter, index) => (
-          <span key={index} className="portfolio-char">
-            {letter}
-          </span>
-        ))}
-      </h1>
+      <h1 className="">PORTFOLIO</h1>
       <div className="bg-white h-2 w-[1100px] mb-20"></div>
       <button className="border border-white text-3xl px-20 py-5 tracking-widest">
         Enter
