92a95895694efecd2369b026c542102ac250b807
Theo-Coquio <coquiotheo@gmail.com>
2025-03-20 02:02:52 +0100

    Hero Section added + patron

--- app/Components/Menu/Menu.tsx
+++ app/Components/Menu/Menu.tsx
@@ -5,6 +5,10 @@ const Menu = () => {
     <div>
       <button>menu</button>
       <span>Portoflioo</span>
+      <span>Services</span>
+      <span>Blog</span>
+      <span>Contact</span>
+      <span>Sign In</span>
     </div>
   );
 };
