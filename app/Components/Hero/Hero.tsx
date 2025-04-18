7b13eb91e74875d99c4a257fdf0637708472e553
Theo-Coquio <coquiotheo@gmail.com>
2025-04-18 18:15:40 +0200

    animation fix hero

--- app/Components/Hero/Hero.tsx
+++ app/Components/Hero/Hero.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import "./hero.css";
 import { bigShouldersDisplayBold } from "@/fonts";
+import gsap from "gsap";
 
 const Hero = () => {
+  useEffect(() => {
+    gsap
+      .timeline()
+      .from("#name span", { duration: 0.5, y: -100, opacity: 0, stagger: 0.1 })
+      .from("#lastname span", {
+        duration: 0.5,
+        y: -100,
+        opacity: 0,
+        stagger: 0.1,
+      });
+  }, []);
+
   return (
     <div
       className={` px-[500px] py-20 ${bigShouldersDisplayBold.className}  flex flex-col items-center justify-center text-[#E0F11F] `}
     >
-      <div className="flex flex-row h-80 w-full justify-center items-start   text-[260px]">
+      <div
+        className="flex flex-row h-80 w-full justify-center items-start text-[260px]"
+        id="name"
+      >
         <span>T</span>
         <span>H</span>
         <span>E</span>
         <span>O</span>
       </div>
-      <div className="flex flex-row h-80 w-full justify-center items-end text-[260px]">
+      <div
+        className="flex flex-row h-80 w-full justify-center items-end text-[260px]"
+        id="lastname"
+      >
         <span>C</span>
         <span>O</span>
         <span>Q</span>
@@ -21,9 +42,7 @@ const Hero = () => {
         <span>I</span>
         <span>O</span>
       </div>
-      <p className="text-3xl">
-        I&apos;m Theo Coquio - 20 years old react developer
-      </p>
+      <p className="text-3xl">I&apos;m Theo - 20 years old react developer</p>
     </div>
   );
 };
